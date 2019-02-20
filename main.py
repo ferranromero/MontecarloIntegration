07a5b83c5af7e118ea563de1660b3fcf4108d797
Ferran Romero <ferran.romero@est.fib.upc.edu>
2019-02-20 09:40:15 +0100

    Bigger display

--- main.py
+++ main.py
@@ -1,22 +1,21 @@
 import turtle
 import random as ran
-import math
 
-turtle.hideturtle()
-turtle.tracer(False)
+
+
 
 
 def drawSquare():
     turtle.penup()
-    turtle.goto(-250, -250)
+    turtle.goto(-350, -350)
     turtle.pendown()
-    turtle.forward(500)
+    turtle.forward(700)
     turtle.left(90)
-    turtle.forward(500)
+    turtle.forward(700)
     turtle.left(90)
-    turtle.forward(500)
+    turtle.forward(700)
     turtle.left(90)
-    turtle.forward(500)
+    turtle.forward(700)
     turtle.left(90)
     turtle.penup()
     turtle.goto(0, 0)
@@ -25,9 +24,9 @@ def drawSquare():
 
 def drawCircle():
     turtle.penup()
-    turtle.goto(0, -250)
+    turtle.goto(0, -350)
     turtle.pendown()
-    turtle.circle(250)
+    turtle.circle(350)
     gotocenter()
 
 
@@ -38,27 +37,29 @@ def gotocenter():
 
 def drawPoint(x, y):
     turtle.penup()
-    #turtle.goto(-250+x,-250+y)
-    turtle.goto(x,y)
+    turtle.goto(x, y)
     turtle.pendown()
     turtle.circle(2)
     turtle.penup()
 
 
+total = int(input("Number of iterations"))
+
+turtle.hideturtle()
+turtle.tracer(False)
 drawSquare()
 drawCircle()
 
 inside = 0
-total = 4000
+
 for i in range(0, total):
     # Generate random x, y in [0, 1].
-    x2 = ran.random()*2 -1
-    y2 = ran.random()*2 -1
+    x2 = ran.random() * 2 - 1
+    y2 = ran.random() * 2 - 1
     turtle.goto(x2, y2)
-    if(turtle.distance(0,0) <= 1.0):
-        drawPoint(x2*250, y2*250)
+    if(turtle.distance(0, 0) <= 1.0):
+        drawPoint(x2 * 350, y2 * 350)
         inside = inside + 1
-        
 pi = (float(inside) / total) * 4
 
 print(pi)
